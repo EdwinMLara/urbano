00d23702be9af453f114f5d511e14f1986e0e995
EdwinMLara <emlara35@gmail.com>
2020-05-20 15:26:01 -0500

    se corrigio el sistema de hasheo en este caso se utilizo openssl

--- core/app/action/hash_data.php
+++ core/app/action/hash_data.php
@@ -1,9 +1,20 @@
 <?php
+
+function encrypt(string $data, string $key, string $method): string{
+    $ivSize = openssl_cipher_iv_length($method);
+        $iv = openssl_random_pseudo_bytes($ivSize);
+        $encrypted = openssl_encrypt($data, $method, $key, OPENSSL_RAW_DATA, $iv);
+    $encrypted = strtoupper(implode(null, unpack('H*', $encrypted)));
+    return $encrypted;
+}
+
 $response = new stdClass();
 if(isset($_POST["data"])){   
-    $hash_data = hash('ripemd160', $_POST["data"]);
+    $key = pack('H*','aaaaaaaaaaaaa');
+    $method = 'aes-256-ecb';
+    $encrypted = encrypt($_POST["data"], $key, $method);
     $response->status = "Sucess";
-    $response->hash = $hash_data;
+    $response->hash = $encrypted;
 }else{
     $response->status = "Error";
     $response->hash = "";
