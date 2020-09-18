eeaf435d7b3f036e121e8f880c45be20f1879b0d
EdwinMLara <emlara35@gmail.com>
2020-09-18 13:09:09 -0500

    creando una api para encryptar datos

---
+++ core/api/construcion-api.php
@@ -0,0 +1,28 @@
+<?php
+include_once '../app/model/ConstruccionData.php';
+include_once '../autoload.php';
+
+function encrypt(string $data, string $key, string $method): string{
+    $ivSize = openssl_cipher_iv_length($method);
+        $iv = openssl_random_pseudo_bytes($ivSize);
+        $encrypted = openssl_encrypt($data, $method, $key, OPENSSL_RAW_DATA, $iv);
+    $encrypted = strtoupper(implode(null, unpack('H*', $encrypted)));
+    return $encrypted;
+}
+
+function decrypt(string $data, string $key, string $method): string{
+    $data = pack('H*', $data);
+    $ivSize = openssl_cipher_iv_length($method);  
+        $iv = $iv = openssl_random_pseudo_bytes($ivSize);
+    $decrypted = openssl_decrypt($data, $method, $key, OPENSSL_RAW_DATA, $iv); 
+    return trim($decrypted);
+}
+
+$key = pack('H*','aaaaaaaaaaaaa');
+$method = 'aes-256-ecb';
+/*$encrypted = encrypt('4937', $key, $method);
+echo $encrypted.'</br>';*/
+$decrypted = decrypt($_GET["encrypted"], $key, $method);
+$licencia = new ConstruccionData();
+$licencia->get_json($decrypted);
+?>
