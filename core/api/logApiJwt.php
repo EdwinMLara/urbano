57b3deb6266f65a764515d44914143afa5e335df
EdwinMLara <emlara35@gmail.com>
2020-09-21 15:00:16 -0500

    api login

---
+++ core/api/logApiJwt.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * {
+     *      "name":"generateToken",
+     *      "param":{
+     *          "email":"emlara35@gmail",
+     *          "pass":"pass123"
+     *       }
+     * }
+     */
+    require_once('funciones.php');
+
+    $api = new Api();
+?>
