fef129ec1765a484be626abafac4098cd1d8f29c
EdwinMLara <emlara35@gmail.com>
2020-09-23 14:42:55 -0500

    comentario para la peticicon con token

--- core/api/logApiJwt.php
+++ core/api/logApiJwt.php
@@ -8,6 +8,16 @@
      *       }
      * }
      */
+
+     /**
+      * token = eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDA4NzYzODYsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTYwMDg3NjQ0NiwidXNlcklkIjoiRWR3aW5NTGFyYSJ9.eqI45ozl5RmeVVgxxPekU1HcV-eBz1DQIoGT37jsHt4
+     * {
+     *      "name":"constructionData",
+     *      "param":{
+     *          "numero_recibo":"cadena"
+     *       }
+     * }
+     */
     require_once('funciones.php');
 
     $api = new Api();
